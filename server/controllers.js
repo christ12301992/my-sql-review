5a9beb48384b9d34e4d474e1c3f463fe96876c11
Christine Ting <christ123092@yahoo.com>
2020-01-10 09:50:16 -0800

    completed code

--- server/controllers.js
+++ server/controllers.js
@@ -1,12 +1,25 @@
-
+const models = require('../database/models.js');
 
 const controllers = {
-  getItems: () => {
-    // TODO
+  getItems: (req, res) => {
+    models.readAll((err, result) => {
+      if (err) {
+        res.status(404).send(err);
+      } else {
+        res.status(200).send(result);
+      }
+    });
     console.log('We got it');
   },
-  postItems: () => {
-    // TODO
+  postItems: (req, res) => {
+    models.addItem(req.body, err => {
+      console.log(req);
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(201).send('item added');
+      }
+    })
     console.log('This is your post request speaking')
   }
 }
